45ea6dc88d9681957434a99daf7e76441628f682
kghugo <hugosum.dev@protonmail.com>
2022-01-07 20:10:24 +0000

    Init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Migration flake for running migration and seeding operation";
+
+  outputs = { ... }:
+    {
+        postgres = {
+            setup = ''
+                touch ./seed.sql
+                mkdir -p ./migrations
+                touch ./.env
+            '';
+
+            migration = ''
+                source ./.env
+                
+                if [ "$POSTGRES_USER" == "" ]; then
+                  POSTGRES_USER="postgres"
+                fi
+                
+                dropdb --if-exists -f -U "$POSTGRES_USER" "$DATABASE_NAME"
+                createdb -U postgres "$DATABASE_NAME"
+            '';
+
+            seed = ''
+                psql -U postgres -d $DATABASE_NAME < ./seed.sql
+            '';
+        };
+    };
+}
